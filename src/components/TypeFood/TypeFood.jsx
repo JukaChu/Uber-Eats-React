a228ab303991ec0cb2c8e7199ac9ce5791a4670a
JukaChu <olexiy.yankevich@gmail.com>
2019-07-03 13:39:35 +0300

    issue #3

--- src/components/TypeFood/TypeFood.jsx
+++ src/components/TypeFood/TypeFood.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './TypeFood.css';
+//понять что и как
+import scrollSpy from 'simple-scrollspy';
+
+window.onload = function () {
+
+    scrollSpy('.restaurant-menu__type', {
+
+        offset: 0, // in pixels
+
+        menuActiveTarget: 'li > a',
+
+        sectionClass: '.menu-type__section-menu',
+
+        activeClass: 'type-food--active',
+
+    })
+
+};
 
 export function TypeFood(props) {
     return (
         <li><a href={props.anchor}
-               className="type_food">{props.type}</a>
+               className="type-food">{props.type}</a>
         </li>
     )
 }
