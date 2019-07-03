ac30ad6376a3a392ce0023497833751a9680a5fc
JukaChu <olexiy.yankevich@gmail.com>
2019-07-03 14:19:52 +0300

    issue #12

--- src/components/Cabinet/Cabinet.jsx
+++ src/components/Cabinet/Cabinet.jsx
@@ -4,11 +4,11 @@ import basket from "../../img/basket.svg";
 
 export function Cabinet() {
     return (
-        <div className="header__cabinet">
+        <div className="cabinet">
             <button className="cabinet__signIn">Sign In</button>
             <button className="cabinet__register">Register</button>
-            <a href="" className="cabinet__basket">
-                <img src={basket} alt="" className="basket__image"/>
+            <a href="" className="cabinet__basket-link">
+                <img src={basket} alt="" className="cabinet__basket-image"/>
             </a>
         </div>
     )
